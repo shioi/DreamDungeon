e972dc14f6889ccc6b04a6545ab7cb5150506bdc
Fut0 <otaku3032001@gmail.com>
2022-03-31 14:46:38 +0530

    Added random movement for enemy AI. This is temporary test AI but can be used as a future spell/effect

--- src/component.rs
+++ src/component.rs
@@ -11,18 +11,7 @@ pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
-pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    ecs.push((
-        Enemy,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
-            },
-        },
-    ));
-}
+
+//randomly move component
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MovingRandomly;
