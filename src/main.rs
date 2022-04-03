0888adc72b99c98f1efcdee0759103b2e43a08bc
Fut0 <otaku3032001@gmail.com>
2022-04-03 11:37:28 +0530

    Fixed game crash when player dies

--- src/main.rs
+++ src/main.rs
@@ -91,18 +91,18 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    embedded_resource!(TERMINAL_FONT, "../resources/Sir_Henry's_32x32.png");
+    let fontname: &str = "dungeonfont.png";
     let context = BTermBuilder::new()
         .with_title("Dream Dungeon")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
-        .with_font("Sir_Henry's_32x32.png", 32, 32)
+        .with_font(fontname, 32, 32)
         .with_font("terminal8x8.png", 8, 8)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "Sir_Henry's_32x32.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "Sir_Henry's_32x32.png")
-        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, fontname)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, fontname)
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, fontname)
         .build()?;
 
     main_loop(context, State::new())
